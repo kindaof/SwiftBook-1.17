aa59fa15b2ab270068313f6876186c3fd65b1f68
Dina^3 <kindaof@bk.ru>
2020-11-03 23:18:24 +0300

    Add files via upload

---
+++ HW1.playground/Pages/Задание 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+// VasilievAG_HW1 i.kasyanenko@swiftbook.ru
+
+//:  [Сылка на тесты]( https://docs.google.com/forms/d/e/1FAIpQLScW1PhBbiwigdcHwR8v1e9mro5N_kJIksb74E1fFqFSZ3Uw-g/viewform)
+
+import UIKit
+
+/*:
+ ## Home Work 1
+ 
+ ### Задание 1
+ 
+ Объявите две строковые константы `firstString` и `secondString`. Присвойте им значения "I can" и "code" (именно такие, ни каких хитростей с лишними пробелами). Выведите в консоль фразу "I can code!" используя эти строковые переменные.
+ */
+let firstString = "I can"
+let secondString = "code"
+print(firstString + " " + secondString + "!")
+
+
+//: Задание 1 из 3  |  [Далее: Задание 2](@next)
---
+++ HW1.playground/Pages/Задание 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/*:
+ ### Задание 3
+ 
+ Необходимо вычислить площадь и периметр прямоугольного треугольника.
+ 
+ >Условия:
+ Катеты прямоугольного треугольника:
+ AC = 8.0, CB = 6.0. Гипотенузу треугольника AB вычисляем при помощи функции `sqrt(Double)`, заменив `Double` суммой квадратов катетов
+ 
+ */
+var cathetusOne = 8.0
+var cathetusTwo = 6.0
+var powerValue: Double = 2
+var Double = pow(cathetusOne,powerValue) + pow(cathetusTwo,powerValue)
+var hypotenuse = sqrt(Double)
+var Square = cathetusOne*cathetusTwo*0.5
+var Perimeter = cathetusOne + cathetusTwo + hypotenuse
+print("Площадь треугольника равна \(Square), а периметр равен \(Perimeter)")
+
