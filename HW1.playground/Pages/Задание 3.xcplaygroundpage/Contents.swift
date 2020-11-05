68eba427de2c3fb0df5e2f8403b95e4aac5f271e
Dina^3 <kindaof@bk.ru>
2020-11-05 11:23:56 +0300

    Update Contents.swift

--- HW1.playground/Pages/Задание 3.xcplaygroundpage/Contents.swift
+++ HW1.playground/Pages/Задание 3.xcplaygroundpage/Contents.swift
@@ -10,12 +10,12 @@ import Foundation
  AC = 8.0, CB = 6.0. Гипотенузу треугольника AB вычисляем при помощи функции `sqrt(Double)`, заменив `Double` суммой квадратов катетов
  
  */
-var cathetusOne = 8.0
-var cathetusTwo = 6.0
-var powerValue: Double = 2
-var Double = pow(cathetusOne,powerValue) + pow(cathetusTwo,powerValue)
-var hypotenuse = sqrt(Double)
-var Square = cathetusOne * cathetusTwo * 0.5
-var Perimeter = cathetusOne + cathetusTwo + hypotenuse
-print("Площадь треугольника равна \(Square), а периметр равен \(Perimeter)")
+let cathetusOne = 8.0
+let cathetusTwo = 6.0
+let powerValue: Double = 2
+let double = pow(cathetusOne,powerValue) + pow(cathetusTwo,powerValue)
+let hypotenuse = sqrt(double)
+let square = cathetusOne * cathetusTwo * 0.5
+let perimeter = cathetusOne + cathetusTwo + hypotenuse
+print("Площадь треугольника равна \(square), а периметр равен \(perimeter)")
 
